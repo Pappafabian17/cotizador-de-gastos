d32cd4e892387ba796008baf622fac0935a03435
Fabian Pappa <fabian.pappa.ext@errepar.com>
2023-06-22 16:58:42 -0300

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,7 @@ function App() {
           animarModal={animarModal}
           setAnimarModal={setAnimarModal}
           guardarGasto={guardarGasto}
+          gastoEditar={gastoEditar}
         />
       )}
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CerrarBtn from "../img/cerrar.svg";
 import Mensaje from "./Mensaje";
 
-const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
-    const [mensaje, setMensaje]= useState("")
+const Modal = ({
+  setModal,
+  animarModal,
+  setAnimarModal,
+  guardarGasto,
+  gastoEditar,
+}) => {
+  const [mensaje, setMensaje] = useState("");
   const [nombre, setNombre] = useState("");
   const [cantidad, setCantidad] = useState("");
   const [categoria, setCategoria] = useState("");
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    if([nombre, cantidad, categoria].includes('')){
-        setMensaje("Todos los campos son obligatorios")
-        setTimeout(()=>{
-            setMensaje("")
-        }, 3000)
-        return;
+
+  useEffect(()=>{
+    if(Object.keys(gastoEditar).length > 0){
+      setNombre(gastoEditar.nombre)
+      setCantidad(gastoEditar.cantidad)
+      setCategoria(gastoEditar.categoria)
     }
-    guardarGasto({nombre, cantidad, categoria})
+  }, [])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([nombre, cantidad, categoria].includes("")) {
+      setMensaje("Todos los campos son obligatorios");
+      setTimeout(() => {
+        setMensaje("");
+      }, 3000);
+      return;
+    }
+    guardarGasto({ nombre, cantidad, categoria });
   };
 
   const ocultarModal = () => {
@@ -24,27 +40,24 @@ const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
     setTimeout(() => {
       setModal(false);
     }, 1000);
-  
-};
+  };
 
-return (
+  return (
     <div className="modal">
       <div className="cerrar-modal">
         <img src={CerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
       </div>
-      <form 
-      onSubmit={handleSubmit}
-      className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+      <form
+        onSubmit={handleSubmit}
+        className={`formulario ${animarModal ? "animar" : "cerrar"}`}
       >
- 
         <legend>Nuevo Gasto</legend>
 
         {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
- 
+
         <div className="campo">
- 
           <label htmlFor="nombre">Nombre Gasto</label>
- 
+
           <input
             id="nombre"
             type="text"
@@ -53,9 +66,8 @@ return (
             onChange={(e) => setNombre(e.target.value)}
           />
 
- 
           <label htmlFor="cantidad">Cantidad</label>
- 
+
           <input
             id="cantidad"
             type="number"
@@ -65,11 +77,11 @@ return (
           />
 
           <label htmlFor="categoria">Categoria</label>
- 
-          <select 
-          id="categoria" 
-          value={categoria}
-          onChange={(e) => setCategoria(e.target.value)}
+
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
           >
             <option value="">--Seleccione--</option>
             <option value="ahorro">Ahorro</option>
